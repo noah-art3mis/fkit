eca4967e61670608a64a4b50230da7e6ce6ff8d8
Gustavo Araujo Costa <gustavo@arcos.org.br>
2024-03-18 05:37:39 -0300

    im going to try the official docs

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
-import { getStorage } from 'firebase/storage';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { writable } from "svelte/store";
+import { type User } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: 'AIzaSyCl6c4gc3Q0PYzeHxbjjOwIYnaGLJPSKeI',
-    authDomain: 'fkit-735d1.firebaseapp.com',
-    projectId: 'fkit-735d1',
-    storageBucket: 'fkit-735d1.appspot.com',
-    messagingSenderId: '539068807986',
-    appId: '1:539068807986:web:7af72da450b6cb435a352b',
+  apiKey: "AIzaSyCl6c4gc3Q0PYzeHxbjjOwIYnaGLJPSKeI",
+  authDomain: "fkit-735d1.firebaseapp.com",
+  projectId: "fkit-735d1",
+  storageBucket: "fkit-735d1.appspot.com",
+  messagingSenderId: "539068807986",
+  appId: "1:539068807986:web:7af72da450b6cb435a352b",
 };
 
 // Initialize Firebase
@@ -19,3 +21,11 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth();
 export const storage = getStorage();
+
+//user store
+export const user = writable<User | null>(null);
+
+onAuthStateChanged(auth, (_user) => {
+  user.set(_user); 
+  console.log(_user);
+});
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,4 +1,26 @@
 <script lang="ts">
+  import { auth, user } from "$lib/firebase";
+  import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+
+  async function signInWithGoogle() {
+    try {
+      const provider = new GoogleAuthProvider();
+      const user = await signInWithPopup(auth, provider);
+      console.log(user);
+    } catch (e) {
+      console.error(e);
+    }
+  }
 </script>
 
-<h2>Sign in</h2>
+{#if $user}
+  <h2>welcome {$user.displayName}</h2>
+  <button class="btn btn-primary" on:click={() => signOut(auth)}
+    >Sign Out</button
+  >
+{:else}
+  <h2>Login</h2>
+  <button class="btn btn-primary" on:click={signInWithGoogle}
+    >Sign in with google</button
+  >
+{/if}
